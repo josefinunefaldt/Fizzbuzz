0d7d2ecdbe3df6f5bb9d4c216e721dc599a09485
josefinunefaldt <josefin6@hotmail.com>
2024-12-30 21:47:45 +0100

    Fizzbuzz version 4

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -1,25 +1,46 @@
-﻿class Program
+﻿using System;
+using System.Linq;
+
+class Program
 {
     static void Main(string[] args)
     {
-
+        if (args.Contains("--sort"))
+        {
+            args = args.OrderBy(x => x).ToArray();
+        }
         foreach (var arg in args)
         {
-
-            if (string.IsNullOrEmpty(arg) || arg.Contains("--sort"))
+            if (string.IsNullOrEmpty(arg) || arg == "--sort")
             {
-                args = [.. args.OrderBy(x => x)];
                 continue;
             }
-            if (int.TryParse(arg, out int number))
+            if (arg.Contains('-'))
             {
+                var rangeParts = arg.Split('-');
+                if (rangeParts.Length == 2 && int.TryParse(rangeParts[0], out int start) && int.TryParse(rangeParts[1], out int end))
+                {
+                    for (int i = start; i <= end; i++)
+                    {
+                        string result = FizzBuzz(i);
+                        Console.WriteLine(result);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid range");
+                }
+            }
 
+
+            else if (int.TryParse(arg, out int number))
+            {
                 string result = FizzBuzz(number);
                 Console.WriteLine(result);
             }
             else
             {
-                Console.WriteLine($"Invalid input: '{arg}'");
+                Console.WriteLine($"Invalid input");
             }
         }
     }
