c45ff6e757b016ab28964740f52bbf6dc030dd3b
josefinunefaldt <josefin6@hotmail.com>
2024-12-30 13:40:39 +0100

    Fizzbuzz version 1

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        if (int.TryParse(args[0], out int number))
+        {
+            string result = FizzBuzz(number);
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
+
+    }
+
+    static string FizzBuzz(int num)
+    {
+        if (num % 3 == 0 && num % 5 == 0)
+            return "FizzBuzz";
+        else if (num % 3 == 0)
+            return "Fizz";
+        else if (num % 5 == 0)
+            return "Buzz";
+        else
+            return num.ToString();
+    }
+}
